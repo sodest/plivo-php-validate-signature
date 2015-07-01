46068a941cc790d69fd0a2541062bde323443d09
sodest <tim.franklin.t@gmail.com>
2015-07-01 19:15:55 -0400

    variable prefix change
    changed variable prefix from get to set for clarification purposes

--- message_url.php
+++ message_url.php
@@ -7,31 +7,31 @@ $auth_id = "XXXXXXXXXXXXXXXXXXXX";
 $auth_token = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
 
 //Get Page URI - Change to "https://" if Needed
-$get_uri = "http://" . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
+$set_uri = "http://" . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
 
 //Get Post Parameters
 $raw_post_data = file_get_contents('php://input');
 $raw_post_array = explode('&', $raw_post_data);
-$get_post_params = array();
+$set_post_params = array();
 foreach ($raw_post_array as $keyval) {
 	$keyval = explode ('=', $keyval);
 	if (count($keyval) == 2)
-		$get_post_params[$keyval[0]] = urldecode($keyval[1]);
+		$set_post_params[$keyval[0]] = urldecode($keyval[1]);
 }
 
 //Get Valid Signature from Plivo
-$get_headers = apache_request_headers();
-foreach ($get_headers as $header_key => $header_value) {
+$set_headers = apache_request_headers();
+foreach ($set_headers as $header_key => $header_value) {
 	if($header_key == "X-Plivo-Signature") {
-    	echo $get_signature .= "$header_value";
+    	echo $set_signature .= "$header_value";
 	}
 }
 
 //Variable Change to Match
-$get_auth_token = $auth_token;
+$set_auth_token = $auth_token;
 
 //Signature Match Returns TRUE (1) - Mismatch Returns FALSE (0)
-$validate_signature = validate_signature($get_uri, $get_post_params, $get_signature, $get_auth_token);
+$validate_signature = validate_signature($set_uri, $set_post_params, $set_signature, $set_auth_token);
 
 //Example XML - Generic Response if FALSE
 if ($validate_signature == false) {
